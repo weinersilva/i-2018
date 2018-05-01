aff88883bab2a0e03adf6cc6d450bebff7edb806
Weiner Silva <weinersilva@gmail.com>
2018-04-30 21:59:54 -0300

    Atividades XML

---
+++ atividadeSupervisionada_Xml/src/com/weinersilva/xml/Exercicio01.java
@@ -0,0 +1,25 @@
+package com.weinersilva.xml;
+
+/**
+ * 
+ * @author Weiner Silva
+ * Criar um programa em Java que recebe como argumento o nome de um 
+ * arquivo no formato CSV (delimitados por ";") e produz um documento 
+ * XML equivalente.
+ *
+ */
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		//funcao que recebe o nome do arquivo CSV e produz um documento xml equivalente.
+		geraXml(args[0]);
+		
+	}
+
+	private static void geraXml(String string) {
+	
+		
+		
+	}
+
+}
