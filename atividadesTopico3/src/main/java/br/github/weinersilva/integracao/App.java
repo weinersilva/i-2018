f4f9edbbefd8f32a42e6affcabbe80511faacde4
Weiner Silva <weinersilva@gmail.com>
2018-05-10 15:09:28 -0300

    Maven project

---
+++ atividadesTopico3/src/main/java/br/github/weinersilva/integracao/App.java
@@ -0,0 +1,13 @@
+package br.github.weinersilva.integracao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
